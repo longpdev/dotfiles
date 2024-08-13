e2933102380bc769adfec5345840608dc32af6b2
Long Pham <longpdev@gmail.com>
2024-08-13 03:16:05 -0600

    small changes

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -146,13 +146,15 @@ screens = [
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
-                widget.Battery(),
+                widget.Battery(
+                    background='#353446'
+                ),
                 widget.Memory(
                     update_interval=2,
                 ),
             ],
             24,
-             border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
