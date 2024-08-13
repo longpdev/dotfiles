40914ee719929b9b60bad8e2f785d35a470116d7
Long Pham <longpdev@gmail.com>
2024-08-13 05:11:04 +0000

    qtile kitty

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -119,7 +119,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
@@ -140,8 +140,8 @@ screens = [
                 widget.QuickExit(),
             ],
             24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+             border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
