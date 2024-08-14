d06a2a4132f73c373e0a1e3ffd97307d0e31ad5f
Long Pham <longpdev@gmail.com>
2024-08-14 02:27:08 -0600

    added gap

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -103,13 +103,13 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4, margin=8),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    # layout.MonadTall(),
+    layout.MonadTall(margin=20),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
