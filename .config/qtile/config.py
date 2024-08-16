d3a4602ef0363f70339bca6fa0d28f2787d537bf
Long Pham <longpdev@gmail.com>
2024-08-16 10:23:46 -0600

    Added rofi command to qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -51,7 +51,8 @@ keys = [
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawn("rofi -show drun"), desc = "Launch Rofi"),
 
     # Install brightnessctl before continue
     Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s 10%+"), desc='brightness UP'),
